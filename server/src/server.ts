28512b1e28ba36d3101482290d1a635344c7f72f
Ewerton Mendonça <netrometro@yahoo.com.br>
2023-04-27 22:23:32 -0300

    Código inicial servidor

---
+++ server/src/server.ts
@@ -0,0 +1,26 @@
+import fastify from 'fastify';
+import cors from '@fastify/cors'
+import * as dotenv from 'dotenv';
+
+dotenv.config()
+
+const server = fastify({
+  logger: true
+});
+
+server.register(cors, {});
+
+server.get('/', async (request, reply) => {
+  return { msg: "Prova Final" };
+});
+
+
+const PORT: any = process.env.PORT;
+
+server.listen({ port: PORT }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
