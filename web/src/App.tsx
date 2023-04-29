eebbc8bba2448d61f85acdb22d16f4e8504ecc4b
Camila Claudino <camila.claudino@upe.br>
2023-04-29 20:21:11 -0300

    criação da navbar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,21 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, NavLink} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ListdragQueens from './pages/DragQueens/listDragQueens';
 import AddDragQueen from './pages/DragQueens/addDragQueen';
-
+import DeleteDragQueen from './pages/DragQueens/deleteDragQueen';
+import NavBar from './navBar';
 export default function App() {
   
   return (
     <>
-      <p>Hello World!</p>
-    
       <div className="app">
         <main> 
+          <NavBar />
           <Router>
             <Routes>
               <Route path="/dragQueens" element={<ListdragQueens/>} />
               <Route path="/dragQueens/create" element={<AddDragQueen/>} />
+              <Route path="/dragQueens/delete/:dragQueenId" element={<DeleteDragQueen/>} />
               <Route path="/" element={<h1>Home</h1>} />
             </Routes>
           </Router>
