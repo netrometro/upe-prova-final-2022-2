0e9930dc4c5b3a9fd92bf59700018ffc5318f963
Camila Claudino <camila.claudino@upe.br>
2023-04-29 20:02:50 -0300

    estilização da view de adicionar entidade

--- web/src/pages/DragQueens/addDragQueen.tsx
+++ web/src/pages/DragQueens/addDragQueen.tsx
@@ -2,6 +2,9 @@
 import react from 'react';
 import { api } from "../../api/axios";
 import { FormEvent, useState, useEffect } from "react";
+import { useParams, useNavigate } from 'react-router-dom';
+import './style.css';
+
 
 interface DragQueens {
   id: number;
@@ -12,6 +15,8 @@ interface DragQueens {
 }
 
 export function AddDragQueen() {
+
+  const navigate = useNavigate();
 
   const [DragQueenName, setDragQueenName] = useState('');
   const [DragQueenSeason, setDragQueenSeason] = useState(0);
@@ -48,38 +53,44 @@ export function AddDragQueen() {
 
 
   return (
-    <div>
-      <form onSubmit={createDragQueen}>
-        <div>
-          <label htmlFor="name"> Nome:</label>
-          <input type="text" name="name" id="name" onChange={event => setDragQueenName(event.target.value)} value={DragQueenName} required />
-        </div>
-        <div>
-          <label htmlFor="season">Temporada:</label>
-          <select name="season" id="season" onChange={event => setDragQueenSeason(Number(event.target.value))} value={DragQueenSeason}>
-            {Array.from({ length: 16 }, (_, i) => i + 1).map((season) => (
-              <option key={season} value={season}>
-                {season}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label htmlFor="winner">Venceu Rupaul's Drag Race?</label>
-          <select name="winner" id="winner" onChange={event => setDragQueenWinner(Boolean(event.target.value))} value={DragQueenWinner.toString()}>
-            <option value="true">Sim</option>
-            <option value="false">Não</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="info">Info:</label>
-          <textarea name="info" id="info" value={DragQueenInfo} onChange={event =>setDragQueenInfo( event.target.value)}></textarea>
-        </div>
+    <div className='tela'>
+      <form onSubmit={createDragQueen} className='form'>
+        <h1 className='title'>Cadastrar Drag Queen</h1>
         <br />
-        <br />
-        <button type="submit">Cadastrar</button>
-        <button type="button" onClick={() => window.location.href = '/dragQueens'}>Cancelar</button>
-
+        <div className='body-queens'>
+          <div >
+            <label htmlFor="name" className='label-queens'> Nome:</label>
+            <br/>
+            <input type="text" name="name" id="name" className='input-queens' onChange={event => setDragQueenName(event.target.value)} value={DragQueenName} required />
+          </div>
+          <div >
+            <label htmlFor="season" className='label-queens'>Temporada:</label>
+            <select name="season" className='input-queens' id="season" onChange={event => setDragQueenSeason(Number(event.target.value))} value={DragQueenSeason}>
+              {Array.from({ length: 16 }, (_, i) => i + 1).map((season) => (
+                <option key={season} value={season}>
+                  {season}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div >
+            <label htmlFor="winner" className='label-queens'>Venceu Rupaul's Drag Race?</label>
+            <select name="winner" className='input-queens' id="winner" onChange={event => setDragQueenWinner(Boolean(event.target.value))} value={DragQueenWinner.toString()}>
+              <option value="true">Sim</option>
+              <option value="false">Não</option>
+            </select>
+          </div>
+          <div >
+            <label htmlFor="info" className='label-queens'>Info:</label>
+            <textarea name="info" className='input-queens' id="info" value={DragQueenInfo} onChange={event =>setDragQueenInfo( event.target.value)}></textarea>
+          </div>
+          <br />
+          <br />
+          <div className='button-group'>
+            <button type="submit" className='button-green'>Cadastrar</button>
+            <button type="button" className='button-blue' onClick={() => navigate( '/dragQueens')}>Voltar</button>
+          </div>
+        </div>
       </form>
 
     </div>
