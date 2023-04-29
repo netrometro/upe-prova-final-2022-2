e834d311a84ca6112c10168c7a17add43ea7f317
elisonmartins <martinselison10@gmail.com>
2023-04-29 14:44:32 -0300

    feat: criaçao das rotas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,15 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/home/index";
 
-export default function App() {
-
+function App() {
   return (
-    <>
-      <p>Hello World!</p>
-    </>
-  );
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+
+      </Routes>
+    </Router>
+  )
 }
+
+export default App
