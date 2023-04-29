cc0140e05b275266926e2f042379a2a92cd1abaa
Camila Claudino <camila.claudino@upe.br>
2023-04-29 17:44:11 -0300

    criação das rotas no front end

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, NavLink} from 'react-router-dom';
+import ListdragQueens from './pages/DragQueens/listDragQueens';
+import AddDragQueen from './pages/DragQueens/addDragQueen';
 
 export default function App() {
-
+  
   return (
     <>
       <p>Hello World!</p>
+    
+      <div className="app">
+        <main> 
+          <Router>
+            <Routes>
+              <Route path="/dragQueens" element={<ListdragQueens/>} />
+              <Route path="/dragQueens/create" element={<AddDragQueen/>} />
+              <Route path="/" element={<h1>Home</h1>} />
+            </Routes>
+          </Router>
+        </main>
+      </div>
     </>
   );
 }
