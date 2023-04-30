a867a78c8891acce7ca44cf9e9ca8a002271aa69
igor leonardo <aionsong456@gmail.com>
2023-04-30 16:53:30 -0300

    correções para exibição da página

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,34 @@
 import './App.css'
-import TaskList from './pages/TaskList'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import ListdragQueens from './pages/DragQueens/listDragQueens';
+import AddDragQueen from './pages/DragQueens/addDragQueen';
+import Escola from "./Escola";
+import Home from "./Home";
+import AnimalPag from "./pages/animal/index";
+import Filmes from "./pages/Filmes/Filmes";
+import ListFilmes from "./pages/Filmes/ListFilmes";
+import TaskList from './pages/TaskList';
 
 export default function App() {
-
-  return <div>
-  <h1>Task List</h1>
-  <TaskList />
-</div>
-
+  return (
+    <>
+      <div className="app">
+        <main>
+          <Router>
+            <Routes>
+              <Route path="/filmes" element={<Filmes />} />
+              <Route path="/ListaFilmes" element={<ListFilmes />} />
+              <Route path="/dragQueens" element={<ListdragQueens/>} />
+              <Route path="/dragQueens/create" element={<AddDragQueen/>} />
+              <Route path='/' element={ <Home /> } />
+              <Route path='/escola' element={ <Escola/> }/>
+              <Route path='/animal' element={<AnimalPag />} />
+              <Route path='/tasks' element ={<TaskList/>}/>
+            </Routes>
+          </Router>
+        </main>
+      </div>
+    </>
+  );
 }
 
