6a524be49fdfc325b719c90fa5e5db920d055f66
Jonas-eng-21 <jonas.eng.software@gmail.com>
2023-04-30 16:44:48 -0300

    Adicinada condicionais ao filtro

--- server/src/controllers/Filmes/findFilmes.ts
+++ server/src/controllers/Filmes/findFilmes.ts
@@ -8,16 +8,24 @@ export const FiltrarFilmes = async (
   reply: FastifyReply
 ) => {
   try {
-    const { busca } = request.query as { busca: string }; // Obter o termo de busca da query parameter
+    const { busca } = request.query as { busca: string };
+
     if (!busca || busca.length === 0) {
       return reply.status(400).send({ success: false, message: 'Por favor, forneça um termo de busca válido.' });
     }
+
     const filmes = await prisma.filme.findMany({
       where: {
         titulo: { contains: busca }, // Filtrar os filmes pelo termo de busca
       },
     });
+
     console.log("lista de filmes", filmes);
+
+    if (filmes.length === 0) {
+      return reply.status(404).send({ success: false, message: 'Não foram encontrados filmes com essa palavra.' });
+    }
+
     reply
       .status(200)
       .send({ success: true, data: filmes, message: "Lista de filmes" });
