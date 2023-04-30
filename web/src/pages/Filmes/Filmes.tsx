3a7fc1a9357098fa6d01f4c18e0cbdc95fd7c9b1
Jonas-eng-21 <jonas.eng.software@gmail.com>
2023-04-30 14:50:09 -0300

    Pequeno ajuste na importação da api no front

--- web/src/pages/Filmes/Filmes.tsx
+++ web/src/pages/Filmes/Filmes.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
-import {api} from "../../api";
+import api from "../../api";
 // import api from "../../services/api";
 import { Link } from "react-router-dom";
 
