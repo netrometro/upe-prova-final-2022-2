e466c479c13ca6e80a8eea244d47f4c26bb45d16
elisonmartins <martinselison10@gmail.com>
2023-04-29 22:17:25 -0300

    fix: troca de nome da pagina home por animal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from "./pages/home/index";
+import Home from "./pages/animal/index";
 
 function App() {
   return (
