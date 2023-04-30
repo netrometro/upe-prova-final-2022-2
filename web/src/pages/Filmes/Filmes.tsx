6a9c812620204fe230e565b7ca88444a89830bbc
Jonas-eng-21 <jonas.eng.software@gmail.com>
2023-04-30 14:53:13 -0300

    Teste para aprovar teste do vercel

--- web/src/pages/Filmes/Filmes.tsx
+++ web/src/pages/Filmes/Filmes.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import api from "../../api";
-// import api from "../../services/api";
 import { Link } from "react-router-dom";
 
 export default function Filmes() {
--- web/src/pages/Filmes/ListFilmes.tsx
+++ web/src/pages/Filmes/ListFilmes.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import api from "../../api";
-// -> LocalHost
-// import api from "../../services/api";
 
 export default function ListFilmes() {
   const [filmes, setFilmes] = useState([]);
