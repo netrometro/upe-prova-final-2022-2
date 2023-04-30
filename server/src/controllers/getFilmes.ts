9f5929e32bfa06034aa7e3993010633c02fd4dca
Jonas-eng-21 <jonas.eng.software@gmail.com>
2023-04-29 22:13:11 -0300

    Construindo no backend a busca da lista de filmes

---
+++ server/src/controllers/getFilmes.ts
@@ -0,0 +1,30 @@
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import * as dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+import { FastifyRequest, FastifyReply } from 'fastify';
+
+dotenv.config();
+
+const server = fastify({
+  logger: true,
+});
+
+server.register(cors);
+
+const prisma = new PrismaClient();
+
+export const buscarFilmes = async (request: FastifyRequest, reply: FastifyReply) => {
+  try {
+    const filmes = await prisma.filme.findMany();
+    console.log("lista de filmes", filmes);
+    reply.status(200).send({success: true, data: filmes, message: "Lista de filmes"});
+  } catch (error) {
+    console.error(error);
+    reply.status(500).send({ success: false, message: "Erro ao buscar filmes" });
+  }
+};
+
+
+export default buscarFilmes;
+
