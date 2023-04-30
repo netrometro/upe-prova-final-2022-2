9a3bcd038cb69293b6cea4bbd2733c72b6963cf7
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-04-29 23:17:11 -0300

    Rotas do Livro

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,29 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, NavLink} from 'react-router-dom';
+import Listlivros from './pages/Livros/listLivros';
+import AddLivros from './pages/Livros/addLivros';
+import DeleteLivro from './pages/Livros/deleteLivro';
+import EditLivro from './pages/Livros/putLivro';
 
 export default function App() {
-
+  
   return (
     <>
       <p>Hello World!</p>
+    
+      <div className="app">
+        <main> 
+          <Router>
+            <Routes>
+              <Route path="/livros" element={<Listlivros/>} />
+              <Route path="/livros/create" element={<AddLivros/>} />
+              <Route path="/livros/delete/:id" element={<DeleteLivro />} />
+              <Route path="/livros/:id" element={<EditLivro />} />
+              <Route path="/" element={<h1>Home</h1>} />
+            </Routes>
+          </Router>
+        </main>
+      </div>
     </>
   );
 }
